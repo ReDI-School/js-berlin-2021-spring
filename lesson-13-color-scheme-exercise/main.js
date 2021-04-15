5822cb951e8c0f59bff1fd30abe8b30c5626f8ca
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2021-04-15 22:28:41 +0200

    add color scheme exercise

---
+++ lesson-13-color-scheme-exercise/main.js
@@ -0,0 +1,27 @@
+/*
+Complete the code for the 2 functions below so that when a button is clicked, the colors of the page are changed accordingly.
+
+For example:
+- one clicks on the "Dark mode" button
+- the darkmode function gets executed
+- the background color of the page is set to the background inside the "darkMode" object
+
+Bonus: create a custom color scheme with color of your choices. Create a new button and a new function to change the color scheme to the one you made.
+*/
+
+let colorSchemes = {
+  lightMode: {
+    background: "#b5b5b5",
+    foreground: "#202124",
+  },
+  darkMode: {
+    background: "#202124",
+    foreground: "#b5b5b5",
+  },
+};
+
+function darkmode() {
+}
+
+function lightmode() {
+}
